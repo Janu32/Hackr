4e02f1d421f5cd8f570633680a1bf7bd9f21b813
Janu32 <67357637+Janu32@users.noreply.github.com>
2020-06-24 13:30:26 +0500

    Update Hacker.py

--- Hacker.py
+++ Hacker.py
@@ -47,7 +47,8 @@ def jalan(z):
 
 
 ##### LOGO #####
-logo = """                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+logo = """    
+
 \033[1;96HHHHHHHHH     HHHHHHHHH               AAA                  CCCCCCCCCCCCCKKKKKKKKK    KKKKKKKEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   
 \033[1;96H:::::::H     H:::::::H              A:::A              CCC::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ER::::::::::::::::R  
 \033[1;96H:::::::H     H:::::::H             A:::::A           CC:::::::::::::::CK:::::::K    K:::::KE::::::::::::::::::::ER::::::RRRRRR:::::R 
